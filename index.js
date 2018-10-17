2c9bfee9b150527b51761997f2430985b48f7b79
Maiko <maikossmaster@gmail.com>
2018-10-17 15:57:54 -0300

    Model de usuário e autoload

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(
   dbConfig.url,
   { useNewUrlParser: true },
+);
+requireDir(dbConfig.modelsPath);
+
+const User = mongoose.model('User');
+User.create(
+  {
+    name: 'Maiko',
+    username: 'maiko',
+    email: 'maikossmaster@gmail.com',
+    password: '123',
+  },
+  () => {
+    console.log('OK');
+  },
 );
 
 app.listen(3000);
