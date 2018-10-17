dfdb3051bab06d72e5febaa6a8e1dd9c18ed7222
Maiko <maikossmaster@gmail.com>
2018-10-17 15:07:49 -0300

    Conectando com o MongoDB

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('express')();
+const mongoose = require('mongoose');
+
+const dbConfig = require('./config/database');
+
+mongoose.connect(
+  dbConfig.url,
+  { useNewUrlParser: true },
+);
+
+app.listen(3000);
